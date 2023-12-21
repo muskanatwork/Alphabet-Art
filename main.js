43432e4789943ba327b74e56e0f77681a2e706c3
Muskan Kumari <muskanatwork23@gmail.com>
2023-12-21 12:39:37 +0530

    I have done responsive

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 let container = document.querySelector(".main-container")
 let crouselImage = document.querySelector(".crousel")
-console.log(crouselImage);
+let dots = document.querySelectorAll(".dot")
+// console.log(crouselImage);
 let alphabet;
 
+// ==================This is use for Crousel Image=====================
 let i = 0;
 let crousel = [
-    "assets/crousel/img 1.webp",
-    "assets/crousel/crousel-img2.jpg",
-    "assets/crousel/crousel-img3.jpg"
+    "assets/crousel/img-1.jpeg",
+    "assets/crousel/img-2.jpeg",
+    "assets/crousel/img-3.jpeg"
 ]
 autoplay = setInterval(function () {
     i++;
+    let selectedDotId = "#dot" + i;
+    let selectedDot = document.querySelector(selectedDotId);
+
+    dots.forEach(dot => dot.style.backgroundColor = "white");
+    selectedDot.style.backgroundColor = "black";
+
     if (i > 2) {
         i = 0;
     }
@@ -18,14 +26,11 @@ autoplay = setInterval(function () {
 }, 1200);
 
 
-let patterns = ["pattern1", "pattern2", "pattern3", "pattern4", "pattern5", "pattern6"]
 
+for (let i = 65, j = 0, k = 0; i <= 90; i++, j++, k++) {
 
-for (let i = 65, j = 0; i <= 90; i++, j++) {
-    if (j >= 6) {
-        j = 0;
-    }
     alphabet = String.fromCharCode(i)
+    // ------------------this is use for front card and back card--------------------
     let frontCard = document.createElement("div")
     let backCard = document.createElement("div")
     // frontCard.setAttribute("class", "front-card")
@@ -40,26 +45,48 @@ for (let i = 65, j = 0; i <= 90; i++, j++) {
     text.innerHTML = alphabet
 
     let image = document.createElement("img")
-    image.setAttribute("src", `assets/alphabet image/${alphabet.toLocaleLowerCase()}.png`)
+    image.setAttribute("src", `assets/alphabetImage/${alphabet.toLowerCase()}.jpeg`)
     image.setAttribute("class", "alphabet-img")
     backCard.appendChild(image)
 
+
+    // ----------------------This is use for Flip card--------------------------------
     let flipCard = document.createElement("div")
     flipCard.appendChild(frontCard)
     flipCard.appendChild(backCard)
     container.appendChild(flipCard)
     flipCard.setAttribute("class", "flip-card")
-    console.log(flipCard);
+    // console.log(flipCard);
 
 
     flipCard.addEventListener("click", function () {
         flipCard.classList.toggle("flipped")
+        let sound = new SpeechSynthesisUtterance();
+        sound.text = soundArr[k]
+        window.speechSynthesis.speak(sound);
+
     })
 
+    // -------------This is use for alphabet sound------------------------
+    let soundArr = ["apple", "ball", "cat", "dog", "elephant", "fish", "goat", "hourse", "igloo", "joker", "kite", "lion"
+        , "monkey", "nose", "octopus", "pig", "queen", "rocket", "snake", "tiger", "umbrela", "ven", "watermelon", "x-ray", "yak", "zebra"]
+
+    // spelling-----------------------
+    let word = document.createElement("p")
+    word.innerHTML = soundArr[k];
+    backCard.appendChild(word)
+    word.setAttribute("class", "alphabet-word")
+
+
+
+
+    // --------------This is use for front pattern-------------------------------
+    if (j >= 6) {
+        j = 0;
+    }
+    let patterns = ["pattern1", "pattern2", "pattern3", "pattern4", "pattern5", "pattern6"]
     frontCard.classList.add("front-card")
     frontCard.classList.add(patterns[j])
-
-
 
 
 
