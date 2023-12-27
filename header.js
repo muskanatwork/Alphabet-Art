784a4588ce36491be933a8f625637cdeae883722
Muskan Kumari <muskanatwork23@gmail.com>
2023-12-27 16:55:08 +0530

    create table

---
+++ header.js
@@ -0,0 +1,23 @@
+let mainHeader = document.getElementById("header")
+let header = `
+<div class="logo">
+    <img src="https://placehold.co/250x100/red/white?text=Logo" alt="Logo">
+</div>
+
+<nav>
+    <div>
+        <a href="stickers/index.html"><img src="../assets/navImages/sticker.png" alt="Stickers"></a>
+        <p>Stickers</p>
+    </div>
+    <div>
+        <a href="workshop/index.html"><img src="../assets/navImages/workshop.png" alt="Workshop"></a>
+        <p>Workshop</p>
+    </div>
+    <div>
+        <a href="contest/index.html"><img src="../assets/navImages/contest.png" alt="Contest"></a>
+        <p>Contest</p>
+    </div>
+</nav>
+`
+mainHeader.innerHTML = header
+console.log(header);
